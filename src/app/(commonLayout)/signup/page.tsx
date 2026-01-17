91055b64633d625b68a11db431906dfff71307b6
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-17 22:29:47 +0600

    signup form, login form

--- src/app/(commonLayout)/signup/page.tsx
+++ src/app/(commonLayout)/signup/page.tsx
@@ -1,5 +1,11 @@
-export default function SignupPage() {
-    return (
-        <div>SignupPage</div>
-    )
+import { SignupForm } from "@/components/signup-form"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
 }
