006bf9441cbbecb13dffd0d32c23d3e609b8b1d8
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-19 21:30:13 +0600

    using aith url from env

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
 import { userService } from "@/services/user.service";
-import { cookies } from "next/headers";
 
 export default async function Home() {
   const {data, error} = await userService.getSession();
