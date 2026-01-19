930f5c09e186d116074b102f4fb0c002dcefb359
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-19 22:28:57 +0600

    roles const file && redirect to login using proxy of not authenticated

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  const {data, error} = await userService.getSession();
-
-  console.log('this is data', data);
-
   return (
     <div>
       <Button>Hello</Button>
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,22 @@
 import { NextRequest, NextResponse } from "next/server"
+import { userService } from "./services/user.service";
+import { roles } from "./constants/roles";
 
 export const proxy = async(request: NextRequest) => {
+    let isAuthenticated = false;
+    let isAdmin = false;
+
+    const {data} = await userService.getSession();
+
+    if (data) {
+        isAuthenticated = true;
+        isAdmin = data?.user?.role === roles.admin
+    }
+
+    if (!isAuthenticated) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+
     return NextResponse.next();
 }
 
