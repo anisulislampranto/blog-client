bca9540891489c563453ee59a379f6a6eb497e5f
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-19 22:08:44 +0600

    user service code cleanup

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,39 +3,42 @@ import { cookies } from "next/headers";
 
 const AUTH_URL = env.AUTH_URL
 
-export const userService = {
-    getSession: async function () {
-        try {
-            const cookieStore = await cookies();
-            const res = await fetch(`${AUTH_URL}/get-session`, {
-                headers: {
-                    Cookie: cookieStore.toString()
-                },
-                cache: "no-store"
-            })
-            const session = await res.json();
+const getSession = async () => {
+    try {
+        const cookieStore = await cookies();
+        const res = await fetch(`${AUTH_URL}/get-session`, {
+            headers: {
+                Cookie: cookieStore.toString()
+            },
+            cache: "no-store"
+        })
+        const session = await res.json();
 
-            if (session?.data === null) {
-                return {
-                    data: null,
-                    error: {
-                        message: 'Session is missing!'
-                    }
-                }
-            }
-
-            return {
-                data: session,
-                error: null
-            }
-        } catch (error) {
-            console.error(error)
+        if (session?.data === null) {
             return {
                 data: null,
                 error: {
-                    message: 'Something went wrong!'
+                    message: 'Session is missing!'
                 }
             }
         }
+
+        return {
+            data: session,
+            error: null
+        }
+    } catch (error) {
+        console.error(error)
+        return {
+            data: null,
+            error: {
+                message: 'Something went wrong!'
+            }
+        }
     }
+}
+
+
+export const userService = {
+    getSession
 }
